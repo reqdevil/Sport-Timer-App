71994e0b3e4c5df381d7ff0ffe8ea8d066bd2717
Berk Er <berkerr.tr@gmail.com>
2021-12-22 17:27:29 +0300

    Update SessionPage.dart
    Session Page created, main elements builded. There will be a ListView in the red area. Waiting for the Data Classes.

--- lib/Pages/SessionPage.dart
+++ lib/Pages/SessionPage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:timer_app/Utilities/AppColor.dart';
 
 class SessionPage extends StatefulWidget {
   const SessionPage({Key? key}) : super(key: key);
@@ -13,16 +14,99 @@ class _SessionPageState extends State<SessionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu),
-              iconSize: 40,
-            ),
-            
-          ],
+      body: Container(
+        color: AppColor.backgroundColor,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              IconButton(
+                alignment: Alignment.centerLeft,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.menu,
+                  color: AppColor.white,
+                ),
+                iconSize: 40,
+              ),
+              const Text(
+                "Your Timer Session",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: const Center(
+                    child: Text(
+                      "This will be placed Set-Round-Timer",
+                      style: TextStyle(
+                        color: AppColor.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("New Session"),
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.horizontal(
+                              left: Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Expanded(child: SizedBox.shrink()),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Load Session"),
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.horizontal(
+                              right: Radius.circular(0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Expanded(child: SizedBox.shrink()),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Save Session"),
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.horizontal(
+                              right: Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
