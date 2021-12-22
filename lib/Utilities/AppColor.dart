fd0b8c89bc28815f51ef85bb6c6016c8b3548ddd
Berk Er <berkerr.tr@gmail.com>
2021-12-22 17:27:53 +0300

    Create AppColor.dart
    App Color class created. All colors will be inherited from here to change across the application.

---
+++ lib/Utilities/AppColor.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color backgroundColor = Color(0xFFBCBDFE);
+  static const Color white = Colors.white;
+}
