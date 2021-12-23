5c29d26427d759bfc42e7539ca26fbf5316e6756
Berk Er <berkerr.tr@gmail.com>
2021-12-23 14:19:13 +0300

    Update AppColor.dart
    Added app colors

--- lib/Utilities/AppColor.dart
+++ lib/Utilities/AppColor.dart
@@ -5,4 +5,8 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color backgroundColor = Color(0xFFBCBDFE);
   static const Color white = Colors.white;
+
+  static const Color primaryColor = Color(0xFF9998FE);
+  static const Color darkPrimaryColor = Color(0xFF6766CD);
+  static const Color secondaryColor = Color(0xFFFF87FF);
 }
